d89dc95b880a0edd6b87e772be45d5adaa13cedf
Travis Boatright <travis@Traviss-MacBook-Pro.local>
2020-10-26 18:02:57 -0400

    created react app, re created styles, and created moods

--- my-classroom/src/App.js
+++ my-classroom/src/App.js
@@ -2,6 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            moods : [
+                {name: "angry", color:"red", votes: 0, emoji: "&#128553;"},
+                {name: "energetic/excited", color:"orange", votes: 0, emoji: "&#128553"},
+                {name: "happy", color:"yellow", votes: 0, emoji: "&#128553"},
+                {name: "calm", color:"green", votes: 0, emoji: "&#128553"},
+                {name: "sad", color:"blue", votes: 0, emoji: "&#128553"},
+                {name: "exhausted/tired", color:"purple", votes: 0, emoji: "&#128553"}
+            ]
+        }
+    }
+uij
+    vote (i) {
+        let newMoods = [...this.state.moods];
+        newMoods[i].votes++;
+        function swap(array, i, j) {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        this.setState({moods: newMoods});
+    }
+
 render (){
   return (
     <div className="App">
@@ -9,33 +35,21 @@ render (){
 
 <h1>How are you feeling?</h1>
 
-<div id="container">
-    
-        <div id="box" className="red">
-            <span role="img" aria-label="upset emoji">&#128553;</span>
-            <h2>Red</h2>
-        </div>
-
-        <div id="box" className="orange">
-            <span role="img" aria-label="sad emoji">&#128533;</span>
-            <h2>Orange</h2>
-        </div>
-
-        <div id="box" className="yellow">
-            <span role="img" aria-label="neutral emoji">&#128528;</span>
-            <h2>Yellow</h2>
-        </div>
-
-        <div id="box" className="green">
-            <span role="img" aria-label="happy emoji">&#128512;</span>
-            <h2>Green</h2>
-        </div>
-
-        <div id="box" className="blue">
-            <span role="img" aria-label="excited emoji">&#129321;</span>
-            <h2>Blue</h2>
-        </div>
-
+<div className="container">
+    {
+        this.state.moods.map((mood, i) =>
+       <button onClick={this.vote.bind(this, i)}>
+            <div key={i} id="box" className={mood.color}>
+                <div className="voteCount">
+                    <h1>{mood.votes}</h1>
+                </div>
+                <div className="moodName">
+                    <h2>{mood.name}</h2>
+                </div>
+            </div>
+            </button>
+        )
+    }
 </div>
 
 </body>
